6b27e11ea1051e0c63be9de4f441bfd6dbcbd571
Stefan Keselj <keseljstefan@gmail.com>
2020-12-30 20:09:36 -0800

    Create write_small_greyscale_image, which uses matplotlib's savefig, instead of opencv's imwrite. This is better for small, greyscale images like those in MNIST.

--- video_transformation_numpy/image_io.py
+++ video_transformation_numpy/image_io.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_image(path: str) -> np.ndarray:
@@ -46,3 +47,45 @@ def write_image(path: str, image_np: np.ndarray) -> np.ndarray:
     assert num_channels == 3, f'{num_channels}'
 
     cv2.imwrite(path, image_np)
+
+
+def write_small_greyscale_image(image: np.ndarray, file_path: str):
+    """Save greyscale <image> to <file_path>.
+
+    This will work best with small images. For larger ones, should use
+    write_image.
+
+    Args:
+        image: np.ndarray with element type np.uint8 and shape (28, 28).
+        file_path: path to write image to.
+    """
+
+    image_len_1, image_len_2 = image.shape
+
+    # Maps np.ndarray elements from an unbound range to [0, 1]. 
+    pixel_normalization_op = plt.Normalize(image.min(), image.max())
+    # Expands the dimensionality of an np.ndarray to have an RGBA column.
+    gray_color_map = plt.cm.gray
+    
+    image = gray_color_map(pixel_normalization_op(image))
+
+
+    ax = plt.gca()
+    
+    ax.set_xlabel('dim 2')
+    ax.set_ylabel('dim 1')
+
+    ax.set_xticks(np.arange(-1, image_len_1-1, 5))
+    ax.set_yticks(np.arange(-1, image_len_2-1, 5))
+    
+    ax.set_xticklabels(np.arange(0, image_len_1, 5))
+    ax.set_yticklabels(np.arange(0, image_len_2, 5))
+
+    ax.grid(color='w', linestyle='-', linewidth=0.5)
+
+    ax.xaxis.tick_top()
+    ax.xaxis.set_label_position('top') 
+
+    ax.imshow(image)
+
+    plt.savefig(file_path, bbox_inches='tight')
